bb56014fadbc55fb4983f4f169d05e64d9d9ee1a
Andrei Kurbatov <167753556+AndreiKurbatov@users.noreply.github.com>
2025-02-09 17:38:34 +0100

    the commit before adding drop-down menu items

--- task2_8/js/main-menu-manager.js
+++ task2_8/js/main-menu-manager.js
@@ -1,8 +1,13 @@
 "use strict";
 const menuOptionsButton = document.getElementById("menu-options-button");
 const headMenuContainer = document.getElementById("head-menu-container");
+const rightMenuButton = document.getElementById("right-menu-button");
+const mainHeaderWrapper = document.getElementById("main-header-wrapper");
 menuOptionsButton.addEventListener("click", () => {
     const totalHeight = document.documentElement.scrollHeight;
     headMenuContainer.style.height = String(totalHeight) + 'px';
     headMenuContainer.style.alignItems = 'start';
+    menuOptionsButton.style.transform = "translateY(10px)";
+    rightMenuButton.style.transform = "translateY(10px)";
+    mainHeaderWrapper.style.transform = "translateY(-3px)";
 });
--- task2_8/ts/main-menu-manager.ts
+++ task2_8/ts/main-menu-manager.ts
@@ -1,8 +1,13 @@
 const menuOptionsButton = document.getElementById("menu-options-button") as HTMLElement;
 const headMenuContainer = document.getElementById("head-menu-container") as HTMLElement;
+const rightMenuButton = document.getElementById("right-menu-button") as HTMLElement;
+const mainHeaderWrapper = document.getElementById("main-header-wrapper") as HTMLElement;
 
 menuOptionsButton.addEventListener("click", () => {
     const totalHeight = document.documentElement.scrollHeight;
     headMenuContainer.style.height = String(totalHeight) + 'px';
     headMenuContainer.style.alignItems = 'start';
+    menuOptionsButton.style.transform = "translateY(10px)";
+    rightMenuButton.style.transform = "translateY(10px)";
+    mainHeaderWrapper.style.transform = "translateY(-3px)"
 })
